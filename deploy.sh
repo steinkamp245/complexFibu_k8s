10973ae406123afb1b128f07ecf7a3abd7802572
steinkamp245 <lutz.steinkamp92@gmail.com>
2018-10-16 15:23:32 +0100

    Updated typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ docker push steinkamp245/multi-server:$SHA
 docker push steinkamp245/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=steinkmap245/multi-client:$SHA
+kubectl set image deployments/client-deployment client=steinkamp245/multi-client:$SHA
 kubectl set image deployments/server-deployment server=steinkamp245/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=steinkamp245/multi-worker:$SHA
